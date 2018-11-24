a184d6094233f0bd4253a6290b9d4bdf7ee7b195
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2018-11-23 23:59:26 -0800

    Fix WiFi connection deduplication script.

--- remove-network-dupes.sh
+++ remove-network-dupes.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-nmcli con | gawk '
-match($0, /(^.* [0-9])( .*)/, capture) {
-    system("nmcli con delete \""capture[1]"\"")
+nmcli con | gawk 'BEGIN {
+    actuallyDelFlag = "dryRun=false"
+    if (ARGV[1] != actuallyDelFlag) {
+        printf "Running in dry run mode. Pass %s to actually delete connections. \n", actuallyDelFlag
+    }
 }
-'
+
+match($0, /(^.* [0-9]+)( .*)/, capture) {
+    if (ARGV[1] == actuallyDelFlag) {
+        system("nmcli con delete \""capture[1]"\"")
+    } else {
+        printf capture[1]"\n"
+    }
+}
+
+END {
+    printf "Done.\n"
+}
+' $@
