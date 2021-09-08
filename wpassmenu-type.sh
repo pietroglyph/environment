3f0d5668e9bc22232399a538453272bff890d7c8
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2021-09-08 10:02:38 -0700

    Move back to sway and port some i3-only amenities

---
+++ wpassmenu-type.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+shopt -s nullglob globstar
+
+typeit=1
+if [[ $1 == "--no-type" ]]; then
+	typeit=0
+	shift
+fi
+
+prefix=${PASSWORD_STORE_DIR-~/.password-store}
+password_files=( "$prefix"/**/*.gpg )
+password_files=( "${password_files[@]#"$prefix"/}" )
+password_files=( "${password_files[@]%.gpg}" )
+
+password=$(printf '%s\n' "${password_files[@]}" | bemenu "$@")
+
+[[ -n $password ]] || exit
+
+if [[ $typeit -eq 0 ]]; then
+	pass show -c "$password" 2>/dev/null
+else
+	wtype $(pass show "$password" | head -n 1) 
+	notify-send "Password typing done" -t 650
+fi
