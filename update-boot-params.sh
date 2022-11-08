caa8d63d6e9612b0b7ba29ee9fde8e2fc7a5bf7d
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2022-11-08 18:59:28 -0500

    Improve ACPI config so that backlight keys work (!)
    I haven't bothered to isolate the two, but it seems like:
     - I don't have to only report for a single windows _OSI call, instead I
       can tell the BIOS that the kernel supports ACPI revision 5.
     - If I set acpi_backlight=vendor then the backlight keys get passed
       through correctly, which is very nice.

--- update-boot-params.sh
+++ update-boot-params.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-efibootmgr -d /dev/nvme0n1 -p 1 -c -L "Arch Linux" -l /vmlinuz-linux -u "root=/dev/nvme0n1p5 rw quiet initrd=/intel-ucode.img initrd=/initramfs-linux.img acpi_osi=! acpi_osi='Windows 2020'"
+efibootmgr -d /dev/nvme0n1 -p 1 -c -L "Arch Linux" -l /vmlinuz-linux -u "root=/dev/nvme0n1p5 rw quiet initrd=/intel-ucode.img initrd=/initramfs-linux.img acpi_rev_override=5 acpi_backlight=vendor intel_iommu=on,igfx_off"
