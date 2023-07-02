d1e170a50049130d65a818e6e2217b4784a46774
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2023-07-01 23:51:16 -0400

    Add acpi_backlight=native to boot params
    Lets kernel modify the backlight (again)

--- update-boot-params.sh
+++ update-boot-params.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-efibootmgr -d /dev/nvme0n1 -p 1 -c -L "Arch Linux" -l /vmlinuz-linux -u "root=/dev/nvme0n1p5 rw quiet initrd=/intel-ucode.img initrd=/initramfs-linux.img acpi_osi=! acpi_osi=Linux acpi_backlight=vendor intel_iommu=on,igfx_off"
+efibootmgr -d /dev/nvme0n1 -p 1 -c -L "Arch Linux (native backlight)" -l /vmlinuz-linux -u "root=/dev/nvme0n1p5 rw quiet initrd=/intel-ucode.img initrd=/initramfs-linux.img acpi_osi=! acpi_osi=Linux acpi_backlight=native intel_iommu=on,igfx_off"
