c462b1c587659979a4dcf6baa36f722e86c8c44e
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2023-07-25 01:03:51 -0400

    Add a small explanation

--- ballsack.sh
+++ ballsack.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+# There's a command called "scrot" that works on X11; an enhanced version, which supports area selection, is called "escrotum". This is thw Wayland version.
 grim -g "$(slurp)" - | wl-copy -t image/png
