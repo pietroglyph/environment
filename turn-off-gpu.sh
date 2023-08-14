7e9339befcecd6821dfb0bce5b2cc7f0cbbed5af
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2023-08-14 19:32:20 -0400

    Attempt to make GPU-disabler work on Olin laptop (it doesn't)

--- turn-off-gpu.sh
+++ turn-off-gpu.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-echo '\_SB.PCI0.PEG0.PEGP._OFF' > /proc/acpi/call
+echo '\_SB.PCI0.PEG1.PEGP._OFF' > /prof/acpi/call # For the Olin laptop
+# echo '\_SB.PCI0.PEG0.PEGP._OFF' > /proc/acpi/call # For the XPS 15
